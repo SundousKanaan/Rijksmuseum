fb25a9bcf13ce89c731004d49a7e79175edbde1c
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-02-15 09:16:00 +0100

    Add files via upload

--- userstory/script/script.js
+++ userstory/script/script.js
@@ -1,43 +1,69 @@
 const API_KEY = 'BI5yOkPW';
 const API_URL = `https://www.rijksmuseum.nl/api/nl/collection?&key=${API_KEY}`;
-// const API_URL = `https://www.rijksmuseum.nl/api/nl/collection?key=BI5yOkPW&ps=100.objectTypes=schilderij`;
+// const APIURL = `https://www.rijksmuseum.nl/api/nl/collection?key=${API_KEY}&ps=100.objectTypes=schilderij`;
 
+
+const schilderij = 'schilderij';
 async function fetchData (){
   const data = fetch(API_URL)
   .then(response => response.json())
   .then(data => {
-    const artObjectsCount = data.artObjects.length;
 
+    const artObjectsCount = data.artObjects.length;
     console.log(artObjectsCount);
 
-    APIdata(data);
+    Top5(data);
   })
 }
 
-
-
-function APIdata(data){
+function Top5(data){
   const top5ArtObjects = data.artObjects.slice(0, 5);
   const imageURLs = top5ArtObjects.map(artObject => artObject.webImage.url);
-    console.log(imageURLs);    
+    console.log(imageURLs);
+
+
+    // makers namen
+    const rr = data.artObjects.map(artObject => artObject.principalOrFirstMaker);
+    console.log(rr);
+
     const listElement = document.createElement('ul');
 
-    imageURLs.forEach((imageURL) => {
-      
-    const itemElement = document.createElement('li');
-    const imageElement = document.createElement('img');
 
-      imageElement.src = imageURL;
-      itemElement.appendChild(imageElement);
-      listElement.appendChild(itemElement);
+
+    imageURLs.forEach((imageURL) => {
+      const itemElement = document.createElement('li');
+      const imageElement = document.createElement('img');
+
+      imageElement.src = imageURL; //img
+      
+
+
+      itemElement.appendChild(imageElement); //li
+      listElement.appendChild(itemElement); //ul
     
     const headerElement = document.querySelector('header');
     headerElement.appendChild(listElement);
   });
-
   console.log(listElement);
 
+
+
+  const imageTitels = top5ArtObjects.map(artObject => artObject.title);
+  console.log(imageTitels);
+
+  var headerimgs = document.querySelectorAll ("header ul li img")
+
+  imageTitels.forEach((imagetitel)=> {
+    for(let i=0; i< headerimgs.length; i++){
+      headerimgs[i].alt = imagetitel;
+    }
+  })
+
 }
+
+
+// const imageTitel = top5ArtObjects.map(artObject => artObject.title);
+// console.log(imageTitel);
 
 
 
